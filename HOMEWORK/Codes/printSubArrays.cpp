af9e5b1bd9c45d18ec4220812619e5582becd6ed
Samarth Rajput <139803280+SamarthRajput@users.noreply.github.com>
2025-03-08 12:48:35 +0530

    Add files via upload

---
+++ HOMEWORK/Codes/printSubArrays.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector<int> v = {1, 2, 3, 4};
+    for(int i = 0; i < v.size(); i++){
+        for(int j = i; j < v.size(); j++){
+            for(int k = i; k <= j; k++){
+                cout << v[k] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
